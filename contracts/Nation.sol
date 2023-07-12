f1182ac883e909a5ac2236473cab3af4c3b921b0
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2023-07-12 17:12:35 +0200

    Update Nation.sol

--- contracts/Nation.sol
+++ contracts/Nation.sol
@@ -1,60 +1,53 @@
 // SPDX-License-Identifier
 pragma solidity ^0.6.0;
 
-import "./HitchensUnorderedKeySet.sol";
+import "./UintSet.sol";
 
 contract Nation {
 
-    using HitchensUnorderedKeySetLib for HitchensUnorderedKeySetLib.Set;
-    HitchensUnorderedKeySetLib.Set widgetSet;
+    using UintSet for UintSet.Set;
+    UintSet.Set set;
 
-    struct WidgetStruct {
-        uint ;
-        bool delux;
-        uint price;
+    struct RecordStruct {
+        string N_NAME;
+        uint N_REGIONKEY;
+        string N_COMMENT;
     }
 
-    mapping(bytes32 => WidgetStruct) widgets;
+    mapping(uint => RecordStruct) things;
 
-    event LogNewWidget(address sender, bytes32 key, string name, bool delux, uint price);
-    event LogUpdateWidget(address sender, bytes32 key, string name, bool delux, uint price);
-    event LogRemWidget(address sender, bytes32 key);
-
-    function newWidget(bytes32 key, string memory name, bool delux, uint price) public {
-        widgetSet.insert(key); // Note that this will fail automatically if the key already exists.
-        WidgetStruct storage w = widgets[key];
-        w.name = name;
-        w.delux = delux;
-        w.price = price;
-        emit LogNewWidget(msg.sender, key, name, delux, price);
+    function newNation(uint N_NATIONKEY, string memory N_NAME, uint N_REGIONKEY, string memory N_COMMENT) public {
+        set.insert(N_NATIONKEY); // Note that this will fail automatically if the key already exists.
+        RecordStruct storage w = things[N_NATIONKEY];
+        w.N_NAME = N_NAME;
+        w.N_REGIONKEY = N_REGIONKEY;
+        w.N_COMMENT = N_COMMENT;
     }
 
-    function updateWidget(bytes32 key, string memory name, bool delux, uint price) public {
-        require(widgetSet.exists(key), "Can't update a widget that doesn't exist.");
-        WidgetStruct storage w = widgets[key];
-        w.name = name;
-        w.delux = delux;
-        w.price = price;
-        emit LogUpdateWidget(msg.sender, key, name, delux, price);
+    function updateWidget(uint N_NATIONKEY, string memory N_NAME, uint N_REGIONKEY, string memory N_COMMENT) public {
+        require(set.exists(N_NATIONKEY), "Can't update a widget that doesn't exist.");
+        RecordStruct storage w = things[N_NATIONKEY];
+        w.N_NAME = N_NAME;
+        w.N_REGIONKEY = N_REGIONKEY;
+        w.N_COMMENT = N_COMMENT;
     }
 
-    function remWidget(bytes32 key) public {
-        widgetSet.remove(key); // Note that this will fail automatically if the key doesn't exist
-        delete widgets[key];
-        emit LogRemWidget(msg.sender, key);
+    function remWidget(uint N_NATIONKEY) public {
+        set.remove(N_NATIONKEY); // Note that this will fail automatically if the key doesn't exist
+        delete things[N_NATIONKEY];
     }
 
-    function getWidget(bytes32 key) public view returns(string memory name, bool delux, uint price) {
-        require(widgetSet.exists(key), "Can't get a widget that doesn't exist.");
-        WidgetStruct storage w = widgets[key];
-        return(w.name, w.delux, w.price);
+    function getWidget(uint N_NATIONKEY) public view returns(string memory N_NAME, uint N_REGIONKEY, string memory N_COMMENT) {
+        require(set.exists(N_NATIONKEY), "Can't get a widget that doesn't exist.");
+        RecordStruct storage w = things[N_NATIONKEY];
+        return(w.N_NAME, w.N_REGIONKEY, w.N_COMMENT);
     }
 
-    function getWidgetCount() public view returns(uint count) {
-        return widgetSet.count();
+    function getCount() public view returns(uint count) {
+        return set.count();
     }
 
-    function getWidgetAtIndex(uint index) public view returns(bytes32 key) {
-        return widgetSet.keyAtIndex(index);
+    function getWidgetAtIndex(uint index) public view returns(uint key) {
+        return set.keyAtIndex(index);
     }
 }
