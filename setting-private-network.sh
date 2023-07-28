97654e01a3953c697514948784ef13f778f8476d
OrpingtonClose <Jakub.Pawlicki@Outlook.com>
2023-07-28 22:54:00 +0200

    Update setting-private-network.sh

--- setting-private-network.sh
+++ setting-private-network.sh
@@ -1,11 +1,25 @@
-vboxmanage import ~/Documents/eth-1.ova --vsys 0 --vmname bootnode
-VBoxManage startvm bootnode
-VBoxManage modifyvm "bootnode" --natpf1 "guestssh,tcp,,2222,,22"
-yes | ssh -p 2222 orpington@127.0.0.1
+#https://github.com/ethereum/go-ethereum/issues/21631
+
+NODENAME=node-two
+SSHPORT=2223
+
+vboxmanage import ~/Documents/eth-1.ova --vsys 0 --vmname $NODENAME
+VBoxManage modifyvm "$NODENAME" --natpf1 "guestssh,tcp,,$SSHPORT,,22"
+VBoxManage startvm $NODENAME
+
+ssh -p $SSHPORT orpington@127.0.0.1
+
+sudo su
 
 mkdir data
-sudo add-apt-repository -y ppa:ethereum/ethereumY
-sudo apt-get install ethereum -y
+wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.24-972007a5.tar.gz
+tar -xvf geth-linux-amd64-1.10.24-972007a5.tar.gz
+mv geth-linux-amd64-1.10.24-972007a5/geth geth
+chmod 777 geth
+mv geth /usr/bin/geth
+
+#sudo add-apt-repository -y ppa:ethereum/ethereum
+#sudo apt-get install ethereum -y
 
 #https://geth.ethereum.org/docs/fundamentals/private-network
 PASS=abc
@@ -13,7 +27,8 @@ NETWORKID=12345
 yes $PASS | geth account new --datadir data &> key.txt
 ADDRESS=$(grep -oP '(?<=0x).*' key.txt)
 echo 0x$ADDRESS
-EXTRADATA=$(echo "$(printf '0%.0s' {1..32})$ADDRESS$(printf '0%.0s' {1..65})")
+#EXTRADATA=$(echo "$(printf '0%.0s' {1..32})$ADDRESS$(printf '0%.0s' {1..65})")
+EXTRADATA=0000000000000000000000000000000000000000000000000000000000000000${ADDRESS}0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 cat <<EOF > genesis.json
 {
   "config": {
@@ -47,15 +62,16 @@ geth init --datadir data genesis.json
 INTERFACE=$(ip route list | grep default | awk '{print $5}')
 IP=$(ifconfig | grep -A 1 $INTERFACE | grep inet | awk '{print $2}')
 echo $INTERFACE $IP
+SYSFILE=eth-node
 
-cat <<EOF > gethboot.service #/etc/systemd/system/myexecuteable.service
+cat <<EOF > $SYSFILE.service #/etc/systemd/system/myexecuteable.service
 [Unit]
-Description=Ethereum go client - boot node
+Description=Ethereum go client
 [Service]
-User=orpington
+User=root
 Type=simple
 WorkingDirectory=/home/orpington
-ExecStart=/usr/bin/geth --datadir data --networkid $NETWORKID --nat extip:$IP --ipcpath /home/orpington/data/geth.ipc
+ExecStart=/usr/bin/geth --datadir data --networkid $NETWORKID --ipcpath /home/orpington/data/geth.ipc
 Restart=on-failure
 RestartSec=5
 [Install]
@@ -63,47 +79,54 @@ WantedBy=default.target
 EOF
 
 #/etc/systemd/system/gethboot.service
-sudo mv gethboot.service /etc/systemd/system/
-sudo systemctl enable gethboot
-sudo systemctl start gethboot
-#journalctl -f -u gethboot
+sudo mv $SYSFILE.service /etc/systemd/system/
+sudo systemctl enable $SYSFILE
+sudo systemctl start $SYSFILE
+#journalctl -f -u $SYSFILE
 #nmap 127.0.0.1
 
-geth attach --exec admin.nodeInfo.enr /home/orpington/data/geth.ipc > ~/enr.txt
-
-cat <<EOF > gethnode1.service #/etc/systemd/system/myexecuteable.service
-[Unit]
-Description=Ethereum go client
-[Service]
-User=orpington
-Type=simple
-WorkingDirectory=/home/orpington
-ExecStart=/usr/bin/geth --datadir data2 --networkid $NETWORKID --port 30305 --bootnodes $(geth attach --exec admin.nodeInfo.enr /home/orpington/data/geth.ipc) --ipcpath /home/orpington/data2/geth.ipc
-Restart=on-failure
-RestartSec=5
-[Install]
-WantedBy=default.target
-EOF
-
-sudo mv gethnode1.service /etc/systemd/system/
-sudo systemctl enable gethnode1
-sudo systemctl start gethnode1
-journalctl -u gethnode1
-
+geth attach /home/orpington/data/geth.ipc #--exec admin.nodeInfo.enode /home/orpington/data/geth.ipc > ~/enr.txt
+#geth attach --exec admin.nodeInfo.enode /home/orpington/data/geth.ipc
+#admin.addPeer("enode://7d5a89459b3c7f3d5a9db53925f245f063d3240b5ce838b0fc2f461822260361d0636d132e81644076ab016cf812cb455b0172606851083b7358d7cd12ab9c71@5.161.220.126:30308")
 exit
 
-scp -P 2222 orpington@127.0.0.1:/home/orpington/{genesis.json,enr.txt} .
 
-vboxmanage import ~/Documents/eth-1.ova --vsys 0 --vmname node1
-VBoxManage modifyvm "node1" --natpf1 "guestssh,tcp,,2223,,22"
-VBoxManage startvm node1
 
-scp -P 2223 {genesis.json,enr.txt} orpington@127.0.0.1:/home/orpington/
 
-ssh -p 2223 orpington@127.0.0.1
 
-mkdir data
-sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo apt-get install ethereum -y
+# cat <<EOF > gethnode1.service #/etc/systemd/system/myexecuteable.service
+# [Unit]
+# Description=Ethereum go client
+# [Service]
+# User=orpington
+# Type=simple
+# WorkingDirectory=/home/orpington
+# ExecStart=/usr/bin/geth --datadir data --networkid $NETWORKID --ipcpath /home/orpington/data/geth.ipc
+# Restart=on-failure
+# RestartSec=5
+# [Install]
+# WantedBy=default.target
+# EOF
+
+# sudo mv gethnode1.service /etc/systemd/system/
+# sudo systemctl enable gethnode1
+# sudo systemctl start gethnode1
+# journalctl -u gethnode1 -f
+
+# exit
+
+# scp -P 2222 orpington@127.0.0.1:/home/orpington/{genesis.json,enr.txt} .
+
+# vboxmanage import ~/Documents/eth-1.ova --vsys 0 --vmname node1
+# VBoxManage modifyvm "node1" --natpf1 "guestssh,tcp,,2223,,22"
+# VBoxManage startvm node1
+
+# scp -P 2223 {genesis.json,enr.txt} orpington@127.0.0.1:/home/orpington/
+
+# ssh -p 2223 orpington@127.0.0.1
+
+# mkdir data
+# sudo add-apt-repository -y ppa:ethereum/ethereum
+# sudo apt-get install ethereum -y
 
 
